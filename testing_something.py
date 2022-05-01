1741f9d63f0f57cb6ebc337cd85ac4028485bd4e
Kingsley Nwoke <nwakingsleych@gmail.com>
2022-05-01 19:38:31 +0200

    added more decorator functions

--- testing_something.py
+++ testing_something.py
@@ -17,3 +17,28 @@ def new_function():
     
 new_function()
 
+def multiply(a, b):
+    print(a*b)
+
+def add(a, b):
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+def divide(a, b):
+    try:
+        return a*b
+    except:
+        return "Division by zero not possible"
+
+
+def take_input(func):
+    print("new1")
+    def user_choice():
+        print("Start1")
+        choice_1, choice_2 = input("Choose two numbers a and b: ").split()
+        func(int(choice_1), int(choice_2))
+        print("Start2")
+    print("New2")
+    return user_choice
